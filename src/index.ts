48ac9d66219e560d0a19824b5c617cf3920eecdc
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-06-28 10:23:31 +0200

    dummy script

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ const i: string = 'Hello, world!'
 
 console.log(i)
 
-fs.mkdirSync('_output')
+const dir = '_output'
 
-fs.writeFileSync('_output/test.json', JSON.stringify({ key: 'value' }), {
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir)
+}
+
+fs.writeFileSync(dir + '/test.json', JSON.stringify({ key: 'value' }), {
   encoding: 'utf8',
   flag: 'w',
 })
