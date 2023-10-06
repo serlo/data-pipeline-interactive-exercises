eda5a4fd1a2fc0da347e39c1877c2612aec6d39e
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-10-06 09:01:51 +0200

    load solved exercises

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { existsSync, mkdirSync, writeFileSync } from 'fs'
 import { updateChanges } from './update-changes'
 import { dateToLocaleDate } from './utils'
+import { loadSolvedExercises } from './load-solved-exercises'
 
 export const startDate = '2023-06-12'
 
@@ -10,6 +11,8 @@ async function run() {
   console.log('script end date', endDate)
 
   await updateChanges()
+
+  await loadSolvedExercises()
 
   if (!existsSync('./_output')) {
     mkdirSync('./_output')
---
+++ src/load-solved-exercises.ts
@@ -0,0 +1,30 @@
+import { existsSync, mkdirSync, writeFileSync } from 'fs'
+import { startDate } from '.'
+import { generateDateList } from './utils'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export async function loadSolvedExercises() {
+  if (!existsSync('./solved')) {
+    mkdirSync('./solved')
+  }
+  const endDate = '2023-06-17'
+  const dates = generateDateList(startDate, endDate)
+
+  for (const date of dates) {
+    const filename = './solved/' + date + '.json'
+    if (!existsSync(filename)) {
+      console.log('load solved exercises for', date)
+      const startOfDay = new Date(date)
+      const endOfDay = new Date(startOfDay.getTime() + 24 * 60 * 60 * 1000)
+      const output = await prisma.exerciseSubmission.findMany({
+        where: {
+          timestamp: { gte: startOfDay, lt: endOfDay },
+          result: 'correct',
+        },
+      })
+      writeFileSync(filename, JSON.stringify(output))
+    }
+  }
+}
